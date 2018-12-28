432f7da8fd8bb1d2f13c43e13eb4d537317ff522
hninwaikhin <wai.khinhninwai@gmail.com>
2018-12-28 16:05:38 +0630

    Updates

---
+++ precache-manifest.98ce081915fd617de3dc23bd13cb149f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74521833d33cc04cacf1",
+    "url": "/reacttest/static/js/runtime~main.74521833.js"
+  },
+  {
+    "revision": "9a0e6ba8bec7323a32dc",
+    "url": "/reacttest/static/js/main.9a0e6ba8.chunk.js"
+  },
+  {
+    "revision": "f4b9e161a2f711d998c3",
+    "url": "/reacttest/static/js/1.f4b9e161.chunk.js"
+  },
+  {
+    "revision": "9a0e6ba8bec7323a32dc",
+    "url": "/reacttest/static/css/main.0b86dbdf.chunk.css"
+  },
+  {
+    "revision": "162f5808f6ff2b6dc10389efde58e928",
+    "url": "/reacttest/index.html"
+  }
+];
